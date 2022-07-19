18c88ccb41c8a9bb8a8795caf14ea8ff47c0f7d8
kzhang <1146976048@qq.com>
2022-07-19 23:18:16 +0800

    Update 7_1.py

--- Day Seven/7_1.py
+++ Day Seven/7_1.py
@@ -1,7 +1,56 @@
 
-# 39. 组合总
+# 39. 组合总和
+# 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+# candidates 中的数字可以无限制重复被选取。
+# 说明：
+# 所有数字（包括 target）都是正整数。
+# 解集不能包含重复的组合。
+# 示例 1:
+# 输入: candidates = [2,3,6,7], target = 7,
+# 所求解集为:
+# [
+#   [7],
+#   [2,2,3]
+# ]
+# 示例 2:
+# 输入: candidates = [2,3,5], target = 8,
+# 所求解集为:
+# [
+#   [2,2,2,2],
+#   [2,3,3],
+#   [3,5]
+# ]
 
 
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        candidates.sort()
+        self.dfs(candidates, target, 0, [], res)
+        return res
+
+    def dfs(self, nums, target, index, path, res):
+        if target < 0:
+            return
+        if target == 0:
+            res.append(path)
+            return
+        for i in range(index, len(nums)):
+            self.dfs(nums, target - nums[i], i, path + [nums[i]], res)
 
 
+if __name__ == '__main__':
+    candidates = [2, 3, 5]
+    target = 8
+    s = Solution()
+    print(s.combinationSum(candidates, target))
+    candidates = [2, 3, 6, 7]
+    target = 7
+    print(s.combinationSum(candidates, target))
+    
 
