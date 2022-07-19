066f13b2522fd0d9780b0f123868763f7a3cf28a
kzhang <1146976048@qq.com>
2022-07-19 23:23:23 +0800

    Update 7_2.py

--- Day Seven/7_2.py
+++ Day Seven/7_2.py
@@ -11,3 +11,24 @@
 # ***************************************
 # https://leetcode.cn/problems/multiply-strings/
 
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == '0' or num2 == '0':
+            return '0'
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        ans = [0] * (len(num1) + len(num2))
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                ans[i + j] += int(num1[i]) * int(num2[j])
+        for i in range(len(ans)):
+            ans[i + 1] += ans[i] // 10
+            ans[i] %= 10
+        while ans[-1] == 0:
+            ans.pop()
+        return ''.join(map(str, ans[::-1]))
+
+
+print(Solution().multiply('2', '3'))
+print(Solution().multiply('123', '456'))
+print("***************************************")
