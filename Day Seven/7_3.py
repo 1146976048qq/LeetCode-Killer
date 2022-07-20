1e1873bc1185b56f038bb79d64b1ed7cfd2a5f8b
kzhang <1146976048@qq.com>
2022-07-20 20:53:56 +0800

    Update 7_3.py

--- Day Seven/7_3.py
+++ Day Seven/7_3.py
@@ -27,3 +27,4 @@ print(Solution().findKthLargest([3,2,1,5,6,4], 2))
 print(Solution().findKthLargest([3,2,3,1,2,4,5,5,6], 4))
 print(Solution().findKthLargest([1], 1))
 print("***************************************")
+
