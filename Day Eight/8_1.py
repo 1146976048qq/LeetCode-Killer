ba64cf4670058aa5acb545bf3c578d56705e8065
kzhang <1146976048@qq.com>
2022-07-21 14:32:01 +0800

    Update 8_1.py

--- Day Eight/8_1.py
+++ Day Eight/8_1.py
@@ -16,3 +16,24 @@
 # 输出: -1
 # ***************************************
 # https://leetcode.cn/problems/find-the-duplicate-number/
+#
+
+class Solution():
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return -1
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return nums[i]
+        return -1
+
+
+print(Solution().findDuplicate([2,3,1,0,2,5,3]))
+print(Solution().findDuplicate([2,4,3,5,1]))
+print(Solution().findDuplicate([0,1,2,3,4,5,6,7,8,9]))
+print("***************************************")
